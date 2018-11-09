e9a990e1c87a9c2a4efc05edad5281bd22cf6059
Sheng Li <l.sheng@haihangyun.com>
2018-11-09 15:50:18 +0800

    download

---
+++ routes/download.js
@@ -0,0 +1,31 @@
+const router = require('koa-router')()
+const fs = require('fs')
+const path = require('path')
+
+const rootPath = path.join(__dirname, '../../')
+
+router.get('/files', async (ctx, next) => {
+    let filePath = path.join(rootPath, '/resource/test')
+    try {
+        // let files = await fs.readdir(filePath)
+        let files = fs.readdirSync(filePath)
+        let avaliable = []
+        if(files&&files.length){
+            files.array.forEach(file => {
+                if(fs.statSync(path.join(filePath, file)).isFile()){
+                    avaliable.push(file)
+                }
+            })
+        }
+        ctx.body = avaliable
+    } catch (e) {
+        ctx.body = e
+    }
+
+})
+
+router.get('/download/:file', async (ctx, next) => {
+
+})
+
+module.exports = router
