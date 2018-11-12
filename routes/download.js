afb1996d3217a9499a16b70a5dd6117c3fb2d7a3
Sheng Li <l.sheng@haihangyun.com>
2018-11-12 17:35:21 +0800

    upload

--- routes/download.js
+++ routes/download.js
@@ -2,19 +2,37 @@ const router = require('koa-router')()
 const fs = require('fs')
 const path = require('path')
 const send = require('koa-send')
+const multer = require('koa-multer')
 
 const rootPath = path.join(__dirname, '../../')
+const resourcesPath = path.join(rootPath, '/resource/')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let request =req
+        let body = req.body
+        cb(null, '/tmp/my-uploads')
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+
+const upload = multer({
+    storage: multer.memoryStorage()
+  });
 
 router.get('/files', async (ctx, next) => {
-    let resourcesPath = path.join(rootPath, '/resource/')
     let avaliable = {
         files: []
     }
+    avaliable.files.indexOf("files")
     try {
         formResource(avaliable, resourcesPath)
-        ctx.body = avaliable
+        // ctx.body = avaliable
+        await ctx.render('download/files', { source: avaliable })
     } catch (e) {
-        ctx.body = e
+        await ctx.render('error', { error: e })
     }
 
 })
@@ -24,12 +42,38 @@ router.get('/download/:file', async (ctx) => {
     await send(ctx, name, { root: rootPath + '/resource' })
 })
 
-let formResource = (dir, resourcesPath) => {
+router.post('/upload', upload.single('file'), async (ctx) => {
     try {
-        let resources = fs.readdirSync(resourcesPath)
+        let upFile = ctx.req.file
+        let storeDir = path.join(resourcesPath, ctx.req.body.path)        
+        createDir(storeDir)
+        fs.writeFileSync(path.join(storeDir, upFile.originalname), upFile.buffer)
+        console.log(`store dir: ${storeDir}`)
+        ctx.body = 'Upload Succeeded!'
+    } catch(e) {
+        ctx.body = e.message
+    }
+    
+})
+
+let createDir = (dirPath) => {
+    let dirs = []
+    while (!fs.existsSync(dirPath)) {
+        dirs.push(dirPath)
+        dirPath = path.join(dirPath, "../")
+    }
+    dirs.reverse()
+    for (dir in dirs) {
+        fs.mkdirSync(dirs[dir])
+    }
+}
+
+let formResource = (dir, savePath) => {
+    try {
+        let resources = fs.readdirSync(savePath)
         if (resources && resources.length) {
             resources.forEach(single => {
-                let resourcePath = path.join(resourcesPath, single)
+                let resourcePath = path.join(savePath, single)
                 let stat = fs.statSync(resourcePath)
                 if (stat.isFile()) {
                     dir.files.push(single)
